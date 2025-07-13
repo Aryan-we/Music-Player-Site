3597bca05cac02016873360dddb18f221ebec50a
Aryan Kumar Dubey <96795631+Aryan-we@users.noreply.github.com>
2025-07-13 19:43:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,74 +1,49 @@
-window.addEventListener("load", function () {
-      setTimeout(() => {
-        document.getElementById("loader").style.opacity = "0";
-        setTimeout(() => {
-          document.getElementById("loader").style.display = "none";
-         
-        }, 1000);
-      }, 4500); 
-    });
+const songs = [
+  "songs/Bijlee Bijlee(PagalWorld.com.cm).mp3",
+  "songs/Kali-Kali-Zulfon-Ke(PaglaSongs).mp3",
+  "songs/Ram Siya Ram(PagalWorld.com.cm).mp3"
+];
 
-var audio=document.getElementById("myaudio");
-var image=document.querySelector("img");
-var nam=document.querySelector("h1");
-var pl=document.getElementById("play");
-var pa=document.getElementById("pause");
-var songs=["Bijlee Bijlee Harrdy Sandhu 128 Kbps.mp3","Chaleya - Jawan_64-(PagalWorld.Ink).mp3","Heeriye - Arijit Singh_64-(PagalWorld.Ink).mp3","Kya-Baat-Ay---Harrdy-Sandhu(PagalWorld).mp3","High Rated Gabru - Guru Randhawa_64-(PagalWorld.Ink).mp3"]
-var images=["img11.jpg","img9.webp","img10.webp","img2.jpg","img1.webp"];
-var names=["Bijlee Bijlee","Chaleya","Heeriye","Kya Baat Ay","High Rated Gabru"];
-i=0;
-l=songs.length;
-pa.style.display="none";
-function play(){
-audio.play();
-pl.style.display="none";
-pa.style.display="inline";
-}
-function pause(){
-audio.pause();
-pl.style.display="inline";
-pa.style.display="none";
-}
-function next(){
-if(i<l-1){
-i=i+1;
-}else{
-    i=0;
-}
-audio.src="./songs/"+songs[i];
-image.src="./photos/"+images[i];
-pl.style.display="inline";
-pa.style.display="none";
-nam.innerHTML=names[i];
-audio.play()
-audio.playbackRate = 1.0;
-audio.muted= false;
-}
-function back(){
-    if(i==0){
-    i=l-1;
-    }else{
-        i=i-1;
-    }
-    audio.src="./songs/"+songs[i];
-    image.src="./photos/"+images[i];
-    pl.style.display="inline";
-    pa.style.display="none";
-    nam.innerHTML=names[i];
+const images = [
+  "photos/img11.jpg",
+  "photos/img12.jpg",
+  "photos/img13.jpg"
+];
+
+const names = [
+  "Bijlee Bijlee",
+  "Kali Kali Zulfon Ke",
+  "Ram Siya Ram"
+];
+
+window.addEventListener("DOMContentLoaded", () => {
+  const audio = document.querySelector("audio");
+  const image = document.getElementById("cover");
+  const nam = document.getElementById("title");
+  const nextBtn = document.getElementById("nextBtn");
+  const prevBtn = document.getElementById("prevBtn");
+
+  let index = 0;
+
+  function updatePlayer() {
+    audio.src = songs[index];
+    image.src = images[index];
+    nam.textContent = names[index];
     audio.play();
-    audio.playbackRate = 1.0;
-    audio.muted= false;
-}
-    audio.onended=function(){
-        pl.style.display="inline";
-        pa.style.display="none";
-   // alert("ended");
-    }
-    audio.addEventListener("playing",function(){
-        pl.style.display="none";
-pa.style.display="inline";
-    })
-audio.onpause=function(){
-pl.style.display="inline";
-pa.style.display="none";
-}
+  }
+
+  function next() {
+    index = (index + 1) % songs.length;
+    updatePlayer();
+  }
+
+  function prev() {
+    index = (index - 1 + songs.length) % songs.length;
+    updatePlayer();
+  }
+
+  nextBtn.addEventListener("click", next);
+  prevBtn.addEventListener("click", prev);
+
+  updatePlayer();
+});
